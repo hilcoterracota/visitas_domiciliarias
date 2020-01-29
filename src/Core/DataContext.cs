be552f4072347eda850be4557bcf6d2aa55a59eb
snwnst <nmartinez@techmedel.com>
2020-01-29 11:17:24 -0600

    bak

--- src/Core/DataContext.cs
+++ src/Core/DataContext.cs
@@ -39,6 +39,53 @@ namespace Sivido.Core
             modelBuilder.Entity<FormularioOpcion>().HasKey(c => new { c.IdFormulario, c.IdOpcion });
             modelBuilder.Entity<TipoVisitaFormulario>().HasKey(c => new { c.IdTipoVisita, c.IdFormulartio });
             modelBuilder.Entity<VisitaRespuesta>().HasKey(c => new { c.IdVisita, c.IdFormulario });
+
+            modelBuilder.Entity<Visita>(entity =>
+            {
+
+                entity.HasOne(d => d.TipoVisita)
+                    .WithMany(p => p.Visitas)
+                    .HasForeignKey(d => d.IdTipoVisita)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Portafolio)
+                    .WithMany(p => p.Visitas)
+                    .HasForeignKey(d => d.IdPortafolio)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Cliente)
+                    .WithMany(p => p.Visitas)
+                    .HasForeignKey(d => d.IdCliente)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Direccion)
+                    .WithMany(p => p.Visitas)
+                    .HasForeignKey(d => d.IdDreccion)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Inspector)
+                    .WithMany(p => p.Visitas)
+                    .HasForeignKey(d => d.IdIsnpector)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+                
+            });
+
+            modelBuilder.Entity<VisitaRespuesta>(entity =>
+            {
+
+                entity.HasOne(d => d.Visita)
+                    .WithMany(p => p.VisitaRespuestas)
+                    .HasForeignKey(d => d.IdVisita)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(d => d.Formulario)
+                    .WithMany(p => p.VisitaRespuestas)
+                    .HasForeignKey(d => d.IdFormulario)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+
+                
+            });
+
         }
 
     }
